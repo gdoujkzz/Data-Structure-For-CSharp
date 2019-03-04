5701a74d3b78641dc25a3710a96172cab666f6a7
张镇 <zhen.zhang@chukou1.com>
2019-03-04 22:39:40 +0800

    Stack init

---
+++ Data-Structure-For-CSharp/StackExample/ArrayStack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackExample
+{
+    /// <summary>
+    /// 顺序栈(第一次的做法)
+    /// </summary>
+    public class ArrayStack<T>
+    {
+        private int _currentIndex;
+        private T[] _item;
+        public int Count { get; set; }
+
+        public ArrayStack()
+        {
+            _item = new T[10];
+            this.Count = 0;
+            _currentIndex = 0;
+        }
+
+        public void Push(T item)
+        {
+            _item[_currentIndex] = item;
+            _currentIndex++;
+            this.Count++;
+        }
+
+        public T Pop()
+        {
+            if (_currentIndex == 0)
+            {
+                return default(T);
+            }
+            var item = _item[_currentIndex - 1];
+            _currentIndex--;
+            _item[_currentIndex] = default(T);
+            this.Count--;
+            return item;
+        }
+    }
+}
---
+++ Data-Structure-For-CSharp/StackExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StackExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayStack<int> arrayStack = new ArrayStack<int>();
+            arrayStack.Push(1);
+            arrayStack.Push(2);
+            arrayStack.Push(3);
+            arrayStack.Push(4);
+            arrayStack.Push(5);
+            arrayStack.Pop();
+            arrayStack.Push(6);
+            arrayStack.Pop();
+            arrayStack.Pop();
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+  
+  
+}
